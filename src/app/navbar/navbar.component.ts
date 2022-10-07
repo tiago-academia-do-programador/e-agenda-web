e425139583968fb7c53ecd04e394be54fcf368ad
Tiago <tiagosantiniathayde@gmail.com>
2022-10-07 16:25:04 -0300

    Implementando funcionalidade de logout

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../auth/services/auth.service';
+import { LocalStorageService } from '../auth/services/local-storage.service';
 import { UsuarioTokenViewModel } from '../auth/view-models/token.view-model';
 import { UsuarioService } from '../core/services/usuario.service';
 
@@ -14,10 +17,26 @@ export class NavbarComponent implements OnInit {
 
   public usuarioLogado$: Observable<UsuarioTokenViewModel | null>;
 
-  constructor(private usuarioService: UsuarioService) { }
+  constructor(
+    private usuarioService: UsuarioService,
+    private authService: AuthService,
+    private localStorageService: LocalStorageService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.usuarioLogado$ = this.usuarioService.usuarioLogado;
   }
 
+  public sair() {
+    this.authService.logout().subscribe({
+      next: () => this.processarLogout()
+    });
+  }
+
+  private processarLogout() {
+    this.usuarioService.logout();
+    this.localStorageService.limparDadosLocais();
+    this.router.navigate(['/conta/autenticar']);
+  }
 }
